238904f5446a6965f86beb8a685880c082769f85
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-11-13 22:12:38 +0700

    implement main.go

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/fikriyusrihan/golang-clean-arch/config"
+	"github.com/fikriyusrihan/golang-clean-arch/infrastructure/datastore"
+	"github.com/fikriyusrihan/golang-clean-arch/infrastructure/router"
+	"github.com/fikriyusrihan/golang-clean-arch/registry"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm/logger"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	config.ReadConfig()
+
+	db := datastore.NewDB()
+	db.Logger.LogMode(logger.Info)
+	
+	r := registry.NewRegistry(db)
+
+	e := echo.New()
+	e = router.NewRouter(e, r.NewAppController())
+
+	fmt.Println("Server listen at http://localhost:" + config.C.Server.Address)
+	if err := e.Start(":" + config.C.Server.Address); err != nil {
+		log.Fatalln(err)
+	}
 }
